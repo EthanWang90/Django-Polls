99be808f0032a269cc397a3a8d5b54d220ee62be
ethan <ethanwang9011@gmail.com>
2019-04-03 07:27:46 +1300

    first migrate

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DataTimeField('data published')
+    pub_data = models.DateTimeField('data published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
