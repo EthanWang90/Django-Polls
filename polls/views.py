631dccff0afea6a984b9d7f8d8b9be254ff4cf3f
ethan <ethanwang9011@gmail.com>
2019-04-03 15:57:43 +1300

    Exception handler

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 
@@ -7,10 +8,17 @@ from django.http import HttpResponse
 
 def index(request):
     lastOneQuestion = Question.objects.filter(id = 1)
-    return HttpResponse(lastOneQuestion)
+    context = {
+        'lastOneQuestion':lastOneQuestion
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % (question_id))
+    try:
+        question = Question.objects.get(id = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     return HttpResponse("You are looking at the results of question %s" % question_id)
