ba20978ebde1fe2dfe39cdc2d730a11378a60260
ethan <ethanwang9011@gmail.com>
2019-04-03 15:10:20 +1300

    add some views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, you are at polls index")
+    lastOneQuestion = Question.objects.filter(id = 1)
+    return HttpResponse(lastOneQuestion)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % (question_id))
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
